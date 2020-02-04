823ca8e2fcfea72ddbd0114c454f70aa75123e9a
MinexAutomation <minex.automation@gmail.com>
2020-02-03 22:53:32 -0800

    Added IServiceCollection extensions.

--- source/R5T.Solida.Solutas/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Solida.Solutas
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Solida.Solutas/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Solutas;
+
+
+namespace R5T.Solida.Solutas
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileSiteSerializer"/> implementation of <see cref="IVisualStudioSolutionFileSiteSerializer"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddVisualStudioSolutionFileSiteSerializer(this IServiceCollection services,
+            ServiceAction<IVisualStudioSolutionFileSerializer> addSolutionFileSerializer)
+        {
+            services
+                .AddSingleton<IVisualStudioSolutionFileSiteSerializer, VisualStudioSolutionFileSiteSerializer>()
+                .RunServiceAction(addSolutionFileSerializer)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileSiteSerializer"/> implementation of <see cref="IVisualStudioSolutionFileSiteSerializer"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileSiteSerializer> AddVisualStudioSolutionFileSiteSerializerAction(this IServiceCollection services,
+            ServiceAction<IVisualStudioSolutionFileSerializer> addSolutionFileSerializer)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileSiteSerializer>(() => services.AddVisualStudioSolutionFileSiteSerializer(
+                addSolutionFileSerializer));
+            return serviceAction;
+        }
+    }
+}
