5b77bdff98e8a3aadc7af89fe862961bee94fc2a
MinexAutomation <minex.automation@gmail.com>
2020-02-03 22:53:07 -0800

    Added the VisualStudioSolutionFileSiteSerializer implementation of IVisualStudioSolutionFileSiteSerializer.

---
+++ source/R5T.Solida.Solutas/Code/Services/Implementations/VisualStudioSolutionFileSiteSerializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using R5T.Cambridge.Types;
+using R5T.Solutas;
+
+
+namespace R5T.Solida.Solutas
+{
+    public class VisualStudioSolutionFileSiteSerializer : IVisualStudioSolutionFileSiteSerializer
+    {
+        private IVisualStudioSolutionFileSerializer VisualStudioSolutionFileSerializer { get; }
+
+
+        public VisualStudioSolutionFileSiteSerializer(IVisualStudioSolutionFileSerializer visualStudioSolutionFileSerializer)
+        {
+            this.VisualStudioSolutionFileSerializer = visualStudioSolutionFileSerializer;
+        }
+
+        public SolutionFileSite Deserialize(string solutionFilePath)
+        {
+            var solutionFile = this.VisualStudioSolutionFileSerializer.Deserialize(solutionFilePath);
+
+            var solutionFileSite = new SolutionFileSite()
+            {
+                SolutionFile = solutionFile,
+                Path = solutionFilePath,
+            };
+            return solutionFileSite;
+        }
+
+        public void Serialize(SolutionFileSite solutionFileSite, bool overwrite = true)
+        {
+            this.VisualStudioSolutionFileSerializer.Serialize(solutionFileSite.Path, solutionFileSite.SolutionFile, overwrite);
+        }
+    }
+}
